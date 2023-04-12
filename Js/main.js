7931e080ba300e794e2f8d9e087adc009ca0fde5
Matheus Lima Cândido <121507091+Matheuslc7@users.noreply.github.com>
2023-04-12 18:15:50 -0300

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,7 @@
+const subtrair = document.querySelector("#subtrair")
+const braco = document.querySelector("#braco")
+const somar = document.querySelector("#somar")
+
+subtrair.addEventListener("click", (evento) => {
+    parseInt(braco.value += 1)
+})
